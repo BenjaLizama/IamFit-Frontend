d2975ab3cefe16313da9a52a3fd473b9359a1aa9
BenjaLizama <be.lizamac@duocuc.cl>
2026-07-06 22:56:18 -0400

    hotfix: Correccion chat invertido

--- src/features/(m.i.a)/screens/MiaChatScreen/MiaChatScreen.styles.ts
+++ src/features/(m.i.a)/screens/MiaChatScreen/MiaChatScreen.styles.ts
@@ -1,6 +1,6 @@
 import { hp, wp } from "@/src/core/utils";
 import { COLOR, UI } from "@/src/theme";
-import { StyleSheet } from "react-native";
+import { Platform, StyleSheet } from "react-native";
 
 export const MiaChatScreenStyles = StyleSheet.create({
   container: {
@@ -27,7 +27,7 @@ export const MiaChatScreenStyles = StyleSheet.create({
     gap: UI.spacing.md,
     paddingHorizontal: UI.spacing.md,
     paddingVertical: UI.spacing.xl,
-    transform: [{ scaleY: -1 }],
+    transform: [{ scaleY: Platform.OS === "ios" ? -1 : 1 }],
   },
 
   emptyEyebrow: {
